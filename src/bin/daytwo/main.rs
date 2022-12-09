30741b9055394aab0a1548e609c8cc41724cfa3b
NightTimeSky04 <ntsbryant@gmail.com>
2022-12-09 12:05:34 +0000

    Day two updated

--- src/bin/daytwo/main.rs
+++ src/bin/daytwo/main.rs
@@ -1,23 +1,81 @@
-// Work In Progress! (early stages!)
+// Work In Progress!
 
 use std::fs;
+use std::str::FromStr;
 
+#[derive(Debug)]
+struct InputError;
+
+impl std::fmt::Display for InputError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "Input contains a character that does not correspond to a hand shape."
+        )
+    }
+}
+impl std::error::Error for InputError {}
+
+#[derive(Debug)]
 enum HandShape {
     Rock,
     Paper,
     Scissors,
 }
 
+impl FromStr for HandShape {
+    type Err = InputError;
+    fn from_str(letter: &str) -> Result<Self, Self::Err> {
+        match letter {
+            "A" | "X" => Ok(HandShape::Rock),
+            "B" | "Y" => Ok(HandShape::Paper),
+            "C" | "Z" => Ok(HandShape::Scissors),
+            _ => Err(InputError),
+        }
+    }
+}
+
+struct Round {
+    opponent_handshape: HandShape,
+    suggested_response: HandShape,
+}
+
+impl FromStr for Round {
+    type Err = InputError;
+
+    fn from_str(letter_pair: &str) -> Result<Self, Self::Err> {
+        let round: Vec<_> = letter_pair.split_whitespace().collect();
+
+        let opponent_handshape = round[0].parse()?;
+        let suggested_response = round[1].parse()?;
+
+        Ok(Round {
+            opponent_handshape,
+            suggested_response,
+        })
+    }
+}
+
 fn main() {
-    let strategy_guide = fs::read_to_string("./src/bin/daytwo/strategy_guide.txt");
+    let strategy_guide_input =
+        fs::read_to_string("./src/bin/daytwo/strategy_guide.txt").expect("Problem reading file.");
+
+    let strategy_guide = strategy_guide_input
+        .lines()
+        .map(|round| round.parse::<Round>().unwrap());
+
+    for round in strategy_guide {
+        println!(
+            "{:?} vs {:?}",
+            round.opponent_handshape, round.suggested_response
+        )
+    }
 }
 
 fn score_hand_shapes(hand_shape: HandShape) -> u32 {
-    let score = match hand_shape {
+    match hand_shape {
         HandShape::Rock => 1,
         HandShape::Paper => 2,
         HandShape::Scissors => 3,
-    };
-
-    score
+    }
 }
