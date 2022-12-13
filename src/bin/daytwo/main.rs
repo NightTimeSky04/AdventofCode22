9dc3e58044c2b7f9692f74e43aa9d09dc501e2f2
NightTimeSky04 <ntsbryant@gmail.com>
2022-12-13 12:10:12 +0000

    Progress made on day 2

--- src/bin/daytwo/main.rs
+++ src/bin/daytwo/main.rs
@@ -35,6 +35,16 @@ impl FromStr for HandShape {
     }
 }
 
+impl HandShape {
+    fn score_hand_shape(self) -> u32 {
+        match self {
+            HandShape::Rock => 1,
+            HandShape::Paper => 2,
+            HandShape::Scissors => 3,
+        }
+    }
+}
+
 struct Round {
     opponent_handshape: HandShape,
     suggested_response: HandShape,
@@ -56,6 +66,16 @@ impl FromStr for Round {
     }
 }
 
+impl Round {
+    fn score_round(self) -> u32 {
+        let hand_shape_score = self.suggested_response.score_hand_shape();
+
+        let total_score = hand_shape_score;
+
+        total_score
+    }
+}
+
 fn main() {
     let strategy_guide_input =
         fs::read_to_string("./src/bin/daytwo/strategy_guide.txt").expect("Problem reading file.");
@@ -69,13 +89,5 @@ fn main() {
             "{:?} vs {:?}",
             round.opponent_handshape, round.suggested_response
         )
-    }
-}
-
-fn score_hand_shapes(hand_shape: HandShape) -> u32 {
-    match hand_shape {
-        HandShape::Rock => 1,
-        HandShape::Paper => 2,
-        HandShape::Scissors => 3,
     }
 }
